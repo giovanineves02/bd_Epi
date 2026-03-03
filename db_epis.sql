7f4724c0e657463c5b57db75fafb33f8327a4de2
Giovani Neves <89802981+giovanineves02@users.noreply.github.com>
2026-03-02 23:01:46 -0300

    Index, Describe

--- db_epis.sql
+++ db_epis.sql
@@ -18,10 +18,13 @@ CREATE TABLE funcionario (
     cpf CHAR(11) UNIQUE NOT NULL,
     cargo VARCHAR(80),
     id_setor INT NOT NULL,
+    id_inspecao INT NOT NULL,
     turno VARCHAR(20) NOT NULL,
     status VARCHAR(20) DEFAULT 'Ativo',
     FOREIGN KEY (id_setor)
-        REFERENCES setor (id_setor)
+        REFERENCES setor (id_setor),
+	FOREIGN KEY (id_inspecao)
+        REFERENCES inspecao_epi (id_inspecao)
 );
 CREATE TABLE epi (
     id_epi INT AUTO_INCREMENT PRIMARY KEY,
@@ -29,7 +32,10 @@ CREATE TABLE epi (
     categoria VARCHAR(60),
     numero_ca VARCHAR(20) NOT NULL,
     fabricante VARCHAR(80),
-    dt_validade DATE NOT NULL
+    dt_validade DATE NOT NULL,
+    id_inspecao INT NOT NULL, 
+	FOREIGN KEY (id_inspecao)
+        REFERENCES inspecao_epi (id_inspecao)
 );
 CREATE TABLE estoque (
     id_estoque INT AUTO_INCREMENT PRIMARY KEY,
@@ -42,5 +48,28 @@ CREATE TABLE estoque (
     FOREIGN KEY (id_fornecedor)
         REFERENCES fornecedor (id_fornecedor)
 );
+CREATE TABLE inspecao_epi (
+    id_inspecao INT AUTO_INCREMENT PRIMARY KEY,
+    data_inspecao date,
+    resultado varchar(20)
+);
+CREATE TABLE entrega (
+    id_entrega INT AUTO_INCREMENT PRIMARY KEY,
+    id_funcionario INT NOT NULL,
+    id_epi INT NOT NULL,
+    id_estoque INT,
+    dt_entrega DATE NOT NULL,
+    dt_devolucao DATE,
+    observacao TEXT,
+    FOREIGN KEY (id_funcionario)
+        REFERENCES funcionario (id_funcionario),
+    FOREIGN KEY (id_epi)
+        REFERENCES epi (id_epi),
+    FOREIGN KEY (id_estoque)
+        REFERENCES estoque (id_estoque)
+);
+
+CREATE INDEX idx_epi_entrega ON entrega(dt_entrega);
+
 
 
